f74dc30b0282a2cc2605d012c770b1e54ec3d515
Jeferson Nascimento <jefersonpn@gmail.com>
2024-08-08 15:36:22 +0200

    missing the 2 last section

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
     <!-- Link to Custom Styles -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- Link to Vite -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <header>
@@ -53,6 +55,7 @@
         <section class="about-section" id="sobre">
             <div class="about-container">
                 <div class="about-content">
+                    <img class="passaporto" src="{{ asset('images/passaporto.png') }}" alt="passaporto">
                     <div class="about-text">
                         <h2>SOBRE NÓS</h2>
                         <div class="divider"></div>
@@ -138,7 +141,7 @@
                     <div class="services-heading-container">
                         <span class="long-green-line"></span>
                         <h2 class="services-title-placeholder">
-                            CONSULTORIA PARA O RECONHECIMENTO DA CIDADANIA ITALIANA
+                            CONSULTORIA PARA O RECONHECIMENTO DA CIDADANIA ITALIANA  {{-- Placeholder --}}
                         </h2>
                         <span class="short-red-line"></span>
                     </div>
@@ -157,7 +160,7 @@
             </div>
         </section>
 
-        <!-- "statua" Section -->
+        <!-- "Statua" Section -->
         <section class="statua-section"></section>
             <div class="statua-container">
                 <div class="statua-content">
@@ -166,7 +169,7 @@
             </div>
         </section>
 
-        <!-- "Services" Section -->
+        <!-- "Parla" Section -->
         <section class="parla-section"></section>
             <div class="parla-container">
                 <div class="parla-content">
@@ -179,6 +182,91 @@
                 </div>
             </div>
         </section>
+
+        <!-- "Testimonial" Section -->
+        <section class="testimonial-section">
+            <div class="left-testimonial-container">
+                <div class="apostrophe-sides">
+                    <img src="{{ asset('images/apostrophe.png') }}" alt="apostrophe">
+                </div>
+                <div class="testimonial-content">
+                    <div class="testimonial-title">
+                        <h2>Regina Márcia Bertoncini Hennemann e Robson Avila Wolff</h2>
+                    </div>
+                    <div class="testimonial-text">
+                        <p>
+                            A empresa nos foi apresentada em 2011 e a partir daí fizemos a cidadania com toda orientação legal e acompanhamento do processo, sempre com profissionalismo e qualidade nos serviços prestados. Agradecemos a Ester e Giovanni, e sua equipe, pela atenção o carisma e carinho que nos tratam até hoje.
+
+    Regina Márcia Bertoncini Hennemann e Robson Avila Wolff
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="meaddle-testimonial-container">
+                <div class="apostrophe-meaddle">
+                    <img src="{{ asset('images/apostrophe.png') }}" alt="apostrophe">
+                </div>
+                <div class="testimonial-content">
+                    <div class="testimonial-title">
+                        <h2>Matheus Tudela de Sà</h2>
+                    </div>
+                    <div class="testimonial-text">
+                        <p>Fui assistido em 2009 por Ester e Giovanni durante o mais importante e complexo projeto da minha carreira : a expatriação. Com uma abordagem firme e diligente me garantiram um accesso espontâneo à minha cidadania. Nada melhor do que dispor de profissionais com tradição e maestria.
+
+Sales Engineering Manager – Cella Retail Solutions
+
+</p>
+                    </div>
+                </div>
+            </div>
+            <div class="right-testimonial-container">
+                <div class="apostrophe-sides">
+                    <img src="{{ asset('images/apostrophe.png') }}" alt="apostrophe">
+                </div>
+                <div class="testimonial-content">
+                    <div class="testimonial-title">
+                        <h2>Erika Mendes Correia</h2>
+                    </div>
+                    <div class="testimonial-text">
+                        <p>Profissionais sérios, experientes, dedicados e apaixonados no suporte ao reconhecimento da cidadania italiana. O processo ocorreu tranquilamente de acordo com a descrição dos serviços prestados. Super recomendo!
+
+Erika Mendes Correia – Clinical Trial Manager at Global Antibiotic R&D Partnership (GARDP) Genève</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- "Contact" Section -->
+        <section class="contact-section" id="contato">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6 contact-title">
+                        <h2>Contato</h2>
+                    </div>
+                    <div class="col-6 contact-form mt-5">
+                        <form>
+                            <div class="mb-3">
+                                <input type="text" class="form-control" id="name" placeholder="Seu nome">
+                            </div>
+                            <div class="mb-3">
+                                <input type="email" class="form-control" id="email" placeholder="Seu e-mail">
+                            </div>
+                            <div class="mb-3">
+                                <textarea class="form-control" id="message" placeholder="Sua mensagem"></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label for="file" class="form-label">Anexar Arquivo</label>
+                                <input type="file" class="form-control" id="file">
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                <button type="submit" class="btn btn-primary">Enviar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </main>
 </body>
 </html>
